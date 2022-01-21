c976b9857d75b564dd3d93cf161bee92a23a699a
Mock, Jason <15215338+mockjv@users.noreply.github.com>
2022-01-20 23:37:33 -0700

    Initial commit of the code base from .NET 6.0.

--- Commands/Defaults.cs
+++ Commands/Defaults.cs
@@ -1,7 +1,8 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.SignalR.Crankier.Commands
 {
@@ -11,5 +12,6 @@ namespace Microsoft.AspNetCore.SignalR.Crankier.Commands
         public static readonly int NumberOfConnections = 10_000;
         public static readonly int SendDurationInSeconds = 300;
         public static readonly HttpTransportType TransportType = HttpTransportType.WebSockets;
+        public static readonly LogLevel LogLevel = LogLevel.None;
     }
 }
